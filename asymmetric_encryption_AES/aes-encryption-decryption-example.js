cc5f38340f3f723b07cb180c4ffd1d5b2c8515cb
apiwatdev <123138365+apiwasooscg@users.noreply.github.com>
2023-06-01 23:22:38 +0700

    done

---
+++ asymmetric_encryption_AES/aes-encryption-decryption-example.js
@@ -0,0 +1,29 @@
+"use strict";
+const crypto = require("crypto");
+
+// Generate a random 256-bit (32-byte) AES key
+const AESkey = crypto.randomBytes(32);
+
+// Generate a random 128-bit (16-byte) initialization vector (IV)
+const iv = crypto.randomBytes(16);
+
+const originalObject = {
+  name: "John Doe",
+  age: 30,
+  email: "johndoe@example.com",
+};
+const textForEncrypt = JSON.stringify(originalObject);
+
+// Encrypt the object using AES-256-CBC
+const cipher = crypto.createCipheriv("aes-256-cbc", AESkey, iv);
+let encrypted = cipher.update(textForEncrypt, "utf8", "hex");
+encrypted += cipher.final("hex");
+console.log("Encrypted :", encrypted);
+
+// Decrypt the encrypted data using AES-256-CBC
+const decipher = crypto.createDecipheriv("aes-256-cbc", AESkey, iv);
+let decrypted = decipher.update(encrypted, "hex", "utf8");
+decrypted += decipher.final("utf8");
+const objectDecrypted = JSON.parse(decrypted);
+
+console.log("Decrypted Object:", objectDecrypted);
---
+++ asymmetric_encryption_AES/aes-encryption-decryption-function-example.js
@@ -0,0 +1,37 @@
+"use strict";
+const crypto = require("crypto");
+
+function encryptObject(text, aesKey, aesIv) {
+  const cipher = crypto.createCipheriv("aes-256-cbc", aesKey, aesIv);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return encrypted;
+}
+
+function decryptObject(encryptObject, aesKey, aesIV) {
+  const decipher = crypto.createDecipheriv("aes-256-cbc", aesKey, aesIV);
+  let decrypted = decipher.update(encryptObject, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return JSON.parse(decrypted);
+}
+
+function run() {
+  const aesKey = crypto.randomBytes(32);
+  const aesIV = crypto.randomBytes(16);
+  const originalObject = {
+    name: "John Doe",
+    age: 30,
+    email: "johndoe@example.com",
+  };
+  const originalObjectText = JSON.stringify(originalObject);
+
+  const objectEncrypted = encryptObject(originalObjectText, aesKey, aesIV);
+
+  console.log("Encrypted :", objectEncrypted);
+
+  const objectDecrypted = decryptObject(objectEncrypted, aesKey, aesIV);
+
+  console.log("Decrypted Object:", objectDecrypted);
+}
+
+run();
